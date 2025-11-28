735ac1769e5920556aa8b66a11bf21b678ca754f
Gianne-Hatdog <heikinnshi@gmail.com>
2025-11-28 08:47:37 +0800

    Update Player.java

--- Player.java
+++ Player.java
@@ -21,43 +21,35 @@ public abstract class Player extends Character {
     }
 
     public void levelUp() {
-        level++;
+    level++;
 
-        maxHealth += 20;
-        health = maxHealth;
+    // Increase stats
+    maxHealth += 20;
+    health = maxHealth;
+    attackDamage += 5;
+    defense += 2;
 
-        
-        attackDamage += 5;
-        defense += 2;
-
-        
-        if (this instanceof Mage) {
-            ((Mage)this).levelUpMana();
-        }
-
-        System.out.println("\n=== LEVEL UP! ===");
-        System.out.println("Level: " + level);
-        System.out.println("Max HP increased to " + maxHealth);
-        if (this instanceof Mage) {
-            System.out.println("Max Mana increased to " + ((Mage)this).getMaxMana());
-        }
-        System.out.println("Attack +5, Defense +2");
-        System.out.println("HP and Mana fully restored!");
-        System.out.println("=================\n");
-
-        if (this instanceof Warrior) {
-            ((Warrior)this).levelUpEnergy();
-        }
-
-        System.out.println("\n=== LEVEL UP! ===");
-        System.out.println("Level: " + level);
-        System.out.println("Max HP increased to " + maxHealth);
-        if (this instanceof Mage) {
-            System.out.println("Max Energy increased to " + ((Warrior)this).getMaxEnergy());
-        }
-        System.out.println("Attack +5, Defense +2");
-        System.out.println("HP and Energy fully restored!");
-        System.out.println("=================\n");
-        
+    // Increase class-specific resource
+    if (this instanceof Mage) {
+        ((Mage)this).levelUpMana();
+    } else if (this instanceof Warrior) {
+        ((Warrior)this).levelUpEnergy();
     }
+
+    // Print level-up info
+    System.out.println("\n=== LEVEL UP! ===");
+    System.out.println("Level: " + level);
+    System.out.println("Max HP increased to " + maxHealth);
+
+    if (this instanceof Mage) {
+        System.out.println("Max Mana increased to " + ((Mage)this).getMaxMana());
+    } else if (this instanceof Warrior) {
+        System.out.println("Max Energy increased to " + ((Warrior)this).getMaxEnergy());
+    }
+
+    System.out.println("Attack +5, Defense +2");
+    System.out.println("HP and class resource fully restored!");
+    System.out.println("=================\n");
+    }
+    
 }
